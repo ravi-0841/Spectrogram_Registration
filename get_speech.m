f8ca5aac6384fb232c0be154ea3d35a3d8e8e570
ravi-0841 <ravishankar0841@gmail.com>
2018-04-23 20:50:57 -0400

    Inverse FFT based recovery added

--- get_speech.m
+++ get_speech.m
@@ -4,14 +4,19 @@ function signal = get_speech(spect_mag,spect_phase,f,window,overlap)
     signal = [];
     for i = 1:size(spect_mag,2)
         fft_seg = spect_mag(:,i) .* exp(1j*spect_phase(:,i));
-        signal_segment = ifft(fft_seg,length(fft_seg));
+        signal_segment = ifft(fft_seg,window_len);
         if isempty(signal)
             signal = signal_segment;
         else
-            length((window_len-overlap_len)*(i-1)+1:(i-1)*window_len)
-            signal((window_len-overlap_len)*(i-1)+1:(i-1)*window_len) = ...
-                (signal((window_len-overlap_len)*(i-1)+1:(i-1)*window_len) + signal(1:overlap_len)) / 2;
-            signal((i-1)*window_len+1:i*window_len-overlap_len) = signal(overlap_len+1:end);
+            overlap_part = (signal(end-overlap_len+1:end) + signal_segment(1:overlap_len)) / 2;
+            non_overlap_part = signal_segment(overlap_len+1:end);
+            
+            signal(end-overlap_len+1:end) = overlap_part;
+            signal = [signal;non_overlap_part];
+            
+%             signal((window_len-overlap_len)*(i-1)+1:(i-1)*window_len) = ...
+%                 (signal((window_len-overlap_len)*(i-1)+1:(i-1)*window_len) + signal_segment(1:overlap_len)) / 2;
+%             signal((i-1)*window_len+1:i*window_len-overlap_len) = signal_segment(overlap_len+1:end);
         end
     end
 end
